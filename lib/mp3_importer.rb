ca50ff8db74657a435bb150ec835f464a2202ac5
Zach  Smith <zdsmith001@gmail.com>
2019-08-27 18:45:26 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,7 +10,7 @@ class MP3Importer
   end
 
   def import
-    @files ||= files.each {|filename|Song.new_by_filename(filename)}
+    files.each {|filename|Song.new_by_filename(filename)}
   end
 
 end
