f009e42c45c0319e222b867a09a920d49882abef
Zach  Smith <zdsmith001@gmail.com>
2019-08-27 17:38:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,7 +23,15 @@ class Artist
     Song.all.select {|song|song.artist == self}
   end
 
-  def find_or_create_by_name
+  def find_or_create_by_name(artist_name)
+    @@all.select do |artist|
+      if artist.name == artist_name
+        artist
+      else
+        new_artist = Artist.new(artist_name)
+        new_artist
+      end
+    end
   end
 
   def print_songs
