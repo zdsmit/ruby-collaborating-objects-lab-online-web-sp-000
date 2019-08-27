6a04601c324be033d9babff365a558cfe0fe4ce4
Zach  Smith <zdsmith001@gmail.com>
2019-08-27 17:21:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,14 +12,7 @@ class Song
   end
 
   def artist=(artist)
-    Artist.all.select do |artist_name|
-      if artist.name == artist
-        self.artist = artist_name
-      else
-        new_artist = Artist.new(artist)
-        new_artist = self.artist
-      end
-    end
+    
   end
 
   def new_by_filename
